16c7caedda894158470f2105f3e36a7289310185
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-02-13 15:06:03 +0000

    created components dir with sub dir and the first component called BookGrid

---
+++ components/home/book-grid/index.js
@@ -0,0 +1,24 @@
+export default function BookGrid() {
+  return (
+    <div>
+      <div id="book-grid" className="mt-20 grid grid-cols-3 bg-red-100 w-full">
+        <div id="book1" className="bg-green-100 p-4">
+          <p className="title mb-4 text-center text-xl font-medium">
+            Game of Thrones
+          </p>
+          <p className="author text-sm text-gray-700 text-center">
+            George RR Martin
+          </p>
+        </div>
+        <div id="book2">
+          <p className="title">Atomic Habits</p>
+          <p className="author">James Clear</p>
+        </div>
+        <div id="book3">
+          <p className="title">Born a Crime</p>
+          <p className="author">Trevor Noah</p>
+        </div>
+      </div>
+    </div>
+  );
+}
