a8a3494ddce6621d469c895e78a352836cfe9d0b
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-02-23 16:23:08 +0000

    implemented like button for book-item

--- components/home/book-grid/book-item.js
+++ components/home/book-grid/book-item.js
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import books from "./books";
 
 export default function BookItem({ title, author }) {
+  const [likes, setLikes] = useState(0);
+
+  const handleIncrementLike = () => {
+    setLikes(likes + 1);
+  };
+
   return (
     <div>
       <div>
         <div className="bg-green-100 p-4">
           <p className="title mb-4 text-center text-xl font-medium">{title}</p>
           <p className="author text-sm text-gray-700 text-center">{author}</p>
+          <p className="text-center text-red-500 font-medium">{likes} likes</p>
+          <div className="flex flex-row justify-center w-full">
+            <button
+              className="bg-red-700 text-white px-3 py-1 rounded-lg"
+              onClick={handleIncrementLike}
+            >
+              Like
+            </button>
+          </div>
         </div>
       </div>
     </div>
